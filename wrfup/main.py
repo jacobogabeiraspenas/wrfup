cc6f215edeaf1a81f52cd7a35ecb9fd0bbf22379
jacobogabeiraspenas <jacobogabeiras@gmail.com>
2024-10-28 23:18:14 +0100

    Add additional checks for ill files or inputs and fix on tile range. After cleaning history and reseting to remote main.

--- wrfup/main.py
+++ wrfup/main.py
@@ -5,7 +5,7 @@ import os
 from wrfup.info import Info
 from wrfup.download import download_tiles, get_tile_names_in_aoi
 from wrfup.ingest import ingest_fields
-from wrfup.utils import clean_up, check_geo_em_file, get_lat_lon_extent
+from wrfup.utils import clean_up, check_geo_em_file, get_lat_lon_extent, first_checks
 from wrfup.calculation import calculate_frc_urb2d, calculate_urb_param
 
 # Configure logging
@@ -45,32 +45,32 @@ def main(argv=None):
     # Create an Info object to store paths and configuration
     info = Info.from_argparse(args)
 
-    # Step 1: Check the geo_em file for required fields and return the dataset
-    geo_em_path = os.path.join(info.work_dir, info.geo_em_file)
-    logging.info("Checking the geo_em file for required fields...")
-    ds = check_geo_em_file(geo_em_path, info.field)
-    if ds is None:
-        logging.error(f"Required field {info.field} is missing from the geo_em file. Exiting...")
+    # Step 1: First checks. Check the geo_em file for required fields and return the dataset
+    if first_checks(info) != 0:
         return 1
 
-    # Step 2: Create field-specific directory inside the work directory
+    # Step 2: Check the geo_em file for required fields and return the dataset
+    geo_em_path = os.path.join(info.work_dir, info.geo_em_file)
+    ds = check_geo_em_file(geo_em_path, info.field)
+    
+    # Step 3: Create field-specific directory inside the work directory
     field_dir = os.path.join(info.temp_dir, info.field)
     if not os.path.exists(field_dir):
         os.makedirs(field_dir)
 
-    # Step 3: Get latitude/longitude extent from geo_em file
+    # Step 4: Get latitude/longitude extent from geo_em file
     lat_min, lat_max, lon_min, lon_max = get_lat_lon_extent(geo_em_path)
 
-    # Step 4: Get tile names based on geo_em file’s extent
+    # Step 5: Get tile names based on geo_em file’s extent
     tile_names = get_tile_names_in_aoi(lat_min, lat_max, lon_min, lon_max, info.field)
 
-    # Step 5: Download the necessary tiles based on field
+    # Step 6: Download the necessary tiles based on field
     if info.field == 'FRC_URB2D':
         merged_tiff_path = download_tiles(tile_names, field_dir, FRC_URB2D_URL)
     elif info.field == 'URB_PARAM':
         merged_tiff_path = download_tiles(tile_names, field_dir, URB_PARAM_URL)
 
-    # Step 6: Perform calculations to prepare data for ingestion
+    # Step 7: Perform calculations to prepare data for ingestion
     # merged_tiff_path = os.path.join(field_dir, 'merged_tiles.tif')
     if info.field == 'FRC_URB2D':
         # logging.info("Calculating FRC_URB2D field...")
@@ -79,7 +79,7 @@ def main(argv=None):
         # logging.info("Calculating URB_PARAM fields...")
         ds = calculate_urb_param(info, ds, merged_tiff_path)
 
-    # Step 7: Write the modified dataset to a new file
+    # Step 8: Write the modified dataset to a new file
     output_geo_em_path = geo_em_path.replace('.nc', f'_{info.field}.nc')
     # logging.info(f"Ingesting {info.field} into the geo_em file...")
     ds.to_netcdf(output_geo_em_path)
